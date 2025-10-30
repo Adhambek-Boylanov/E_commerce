3b41add28bf70bcfdb835b551b5102e3551d1aca
adham <=adhambekboylanov@gmail.com>
2025-10-30 20:15:02 +0500

    add category

--- configapp/admin.py
+++ configapp/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from configapp.models import User,Product,Review,FlashSale
+from configapp.models import User,Product,Review,FlashSale,Category
 admin.site.register(User)
 admin.site.register(Product)
 admin.site.register(Review)
 admin.site.register(FlashSale)
+admin.site.register(Category)
